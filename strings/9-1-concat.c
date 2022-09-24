5b56281b5100f6c6c524ca82bdefa30f3f04773e
ahmed-kaif <kaifkhan.rcs@gmail.com>
2022-09-24 10:31:56 +0600

    Added new programs

--- strings/9-1-concat.c
+++ strings/9-1-concat.c
@@ -6,11 +6,11 @@ void concat(char result[],const char str1[],int n1,
 {
     int i,j;
                 ///copy str1 to result
-    for(i=0; i<n1; ++i)
+    for(i = 0; i < n1; ++i)
         result[i] = str1[i];
 
                 ///copy str2 to result
-    for(j=0;j<n2;++j)
+    for(j = 0; j < n2; ++j)
         result[n1+j] = str2[j];
 }
 int main(void)
@@ -23,10 +23,10 @@ int main(void)
 
      char s3[11];
      int i;
-       concat(s3,s1,5,s2,6);
+       concat(s3, s1, 5, s2, 6);
 
-     for (i= 0; i<11; i++)
-                    printf("%c",s3[i]);
+     for (i = 0; i < 11; i++)
+		  printf("%c",s3[i]);
      printf("\n");
 
      return 0;
--- strings/9-2-string-length.c
+++ strings/9-2-string-length.c
@@ -1,22 +1,22 @@
-///function to count character
-
+///Program 9.2 - function to count characters in a string.
 #include<stdio.h>
-int stringLength(const char string[])
+
+int stringLength(const char string[]) // **const** bcz it doesn't change the array
 {
     int count = 0;
 
-    while(string[count] != '\0')
+    while(string[count] != '\0') // '\0' is called the null character which points the end of a string
         ++count;
 
     return count;
 }
 int main(void)
 {
-    int stringLength(const char string[]);
+    int stringLength(const char string[]); // prototype declaration
 
     const char word1[] = {'a','s','t','e','r','\0'};
     const char word2[] = {'a','t','\0'};
-    const char word3[] = {'a','w','e','\0'};
+    const char word3[] = {'\0'};
 
     printf("%i %i %i\n",stringLength(word1),stringLength(word2),stringLength(word3));
 
